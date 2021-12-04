f7293d6500f3a60f4cb3edfe3065b2b738c3865e
JannesM <jannes.meinders@ewe.net>
2021-12-04 14:58:51 +0100

    updated copyright header

--- primitives/Block.py
+++ primitives/Block.py
@@ -1,1 +1,3 @@
-# Block structure
+# Copyright (c) 2021 JannesM
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
--- primitives/Transaction.py
+++ primitives/Transaction.py
@@ -1,1 +1,3 @@
-# Transaction structure
+# Copyright (c) 2021 JannesM
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
