2990a5e6f94884302e5c508ed452095fbe3aad85
JannesM <jannes.meinders@ewe.net>
2021-12-06 17:22:33 +0100

    added locateTxOutput()

--- chain.py
+++ chain.py
@@ -7,18 +7,18 @@ import consensus.BlockValidationRules as BlockValidator
 
 from primitives.Transaction import Transaction
 
-class Chain:
+class Blockchain:
     """ Data class that keeps track of the current state of the blockchain
     """
 
-    blockchain: list[Block] = []
+    chain: list[Block] = []
 
     def __init__(self) -> None:
-        self.blockchain.append(BlockValidator.genesis())
+        self.chain.append(BlockValidator.genesis())
 
 
     def locateTxOutput(self, height, hash, index) -> Transaction:
-        txs = self.blockchain[height].transactions
+        txs = self.chain[height].transactions
         for tx in txs:
             if tx.hash == hash:
                 if len(tx.outputs) > index:
