2c76656f3ad8d27883ab402706a25561b7e9f01a
JannesM <jannes.meinders@ewe.net>
2021-12-10 20:35:37 +0100

    simplefied Transaction class; added block serialization

--- primitives/Block.py
+++ primitives/Block.py
@@ -11,22 +11,29 @@ class Block:
     """
     
     # header
-    height: int
-    timestamp: float
-    prevHash: bytes
-    merkle_root: bytes
-    nonce: int
-    hash: bytes
+    height: bytes # (20)
+    timestamp: bytes # (20)
+    prevHash: bytes # (32)
+    merkle_root: bytes # (32)
+    nonce: bytes # (20)
+    hash: bytes # (32)
 
     # transactions
     transactions: list[Transaction] = [] # referring to primitives/Transaction
     
-    def __init__(self, height, prevHash, transactions) -> None:
-        self.height = height
-        self.timestamp = time.time()
+    def __init__(self, height, prevHash, transactions, timestamp=None, nonce=0) -> None:
+        
+        self.height = str(height).encode('utf-8')
+        
+        if timestamp is None:
+            self.timestamp = str(time.time()).encode('utf-8')
+        else:
+            self.timestamp = str(timestamp).encode('utf-8')
+        
+        
         self.prevHash = prevHash
         self.transactions = transactions
-        self.nonce = 0
+        self.nonce = str(nonce).encode('utf-8')
 
         self.merkle_root = self.calculate_merkle_root()
         self.hash = self.calculate_hash()
@@ -51,3 +58,17 @@ class Block:
     
         hashStr = str(self.height).encode('utf-8') + str(self.timestamp).encode('utf-8') + str(self.nonce).encode('utf-8') + self.merkle_root + self.prevHash
         return hashlib.sha256(hashStr).digest()
+
+    def get_timestamp(self) -> float:
+        """Function that converts unix timestamp from bytes to float
+
+        Returns unix timestamp as a floating point number
+        """
+        return float(self.timestamp.decode())
+
+    def get_height(self) -> float:
+        """Function that converts unix timestamp from bytes to float
+
+        Returns unix timestamp as a floating point number
+        """
+        return int(self.height.decode())        
--- serialization.py
+++ serialization.py
@@ -3,9 +3,18 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 from primitives.Transaction import Transaction
+from primitives.Block import Block
 import base64
 
-FLAG_TX      = b'\x10'
+FLAG_TX             = b'\xFF\xF0'
+FLAG_TX_TOKEN_ASSET = b'\xFF\xF1'
+FLAG_TX_HASH_ASSET  = b'\xFF\xF2'
+FLAG_TX_ATTR        = b'\xFF\xF3'
+FLAG_TX_ARRAY       = b'\xFF\xF4'
+
+FLAG_BLOCK          = b'\xFF\xF5'
+FLAG_BLOCK_ATTR     = b'\xFF\xF6'
+
 
 def get_type(data: bytes) -> bytes:
     """Function that analyzes the data type
@@ -20,23 +29,31 @@ def get_type(data: bytes) -> bytes:
     
     return data[:1]
 
-def encodeTx(tx: Transaction) -> bytes:
+def encodeTx(tx: Transaction, b64encoding=True) -> bytes:
     """Function to convert a transaction object into base64 encoded data
 
     Args:
         tx (Transaction): the transaction to convert
 
-    Returns a byte string containing the base64 encoded conetent
+    Returns a byte string containing the base64 encoded content
     """
     # timestamp (20), script_pub_key (33), sender (34), receiver (34), asset(1+ 10|32), hash (32), script_sig (64)
-    encoded = FLAG_TX + tx.timestamp + tx.script_pub_key
-    encoded += tx.sender + tx.receiver + tx.asset
-    encoded += tx.hash + tx.script_sig
+    encoded = FLAG_TX 
+    encoded += FLAG_TX_ATTR + tx.timestamp
+    encoded += FLAG_TX_ATTR + tx.script_pub_key
+    encoded += FLAG_TX_ATTR + tx.sender
+    encoded += FLAG_TX_ATTR + tx.receiver
+    encoded += FLAG_TX_ATTR + tx.asset
+    encoded += FLAG_TX_ATTR + tx.hash
+    encoded += FLAG_TX_ATTR + tx.script_sig
     
-    encoded = base64.b64encode(encoded)
-    return encoded
+    if b64encoding:
+        encoded = base64.b64encode(encoded)
+        return encoded
+    else:
+        return encoded
 
-def decodeTx(data: bytes) -> Transaction:
+def decodeTx(data: bytes, b64decoding=True) -> Transaction:
     """Function to convert base64 encoded data into a transaction object
 
     Args:
@@ -45,40 +62,92 @@ def decodeTx(data: bytes) -> Transaction:
     Returns a transaction object
     """
 
-    data = base64.b64decode(data)
+    if b64decoding:
+        data = base64.b64decode(data)
     
-    if data[:1] != FLAG_TX:
+    if data[:2] != FLAG_TX:
+        print('[Serialization]: Not a transaction! FLAG is', data[:2])
         return None
     
-    ts = float(data[1:21].decode())
-    script_pub_key = data[21:54]
-
-    sender = data[54:88]
-    receiver = data[88:122]
+    data = data.split(FLAG_TX_ATTR)
     
+    ts = data[1].decode()
+    script_pub_key = data[2]
+    sender = data[3]
+    receiver = data[4]
+    asset = data[5]
+    hash = data[6]
+    script_sig = data[7]
+
     tx = Transaction(script_pub_key, sender, receiver, timestamp=ts)
+    tx.asset = asset
+    tx.hash = hash
+    tx.script_sig = script_sig
 
-    if data[122:123] == b'\xa1':
-        # it's a token
-        tx.generate_token_asset(float(data[123:133].decode()))
-        tx.hash = data[133:165]
-        tx.script_sig = data[165:]
-
-    elif data[122:123] == b'\xa2':
-        # it's an asset represented by a hash
-        tx.generate_hash_asset(data[123:155])
-        tx.hash = data[155:187]
-        tx.script_sig = data[187:]
-
-    else:
-        print('[Serialization]: Wrong asset format!')
-        return None
-
-    
-    if tx.calculate_hash() != tx.hash:
-        print('[Serialization]: Hash comperation failed!')
-        return tx
 
     return tx
 
+def encodeBlock(block: Block) -> bytes:
+    """Function to convert a block object into base64 encoded data
+
+    Args:
+        block (Block): the block to convert
+
+    Returns a byte string containing the base64 encoded content
+    """
+
+    encoded = FLAG_BLOCK
+    encoded += FLAG_BLOCK_ATTR + block.height
+    encoded += FLAG_BLOCK_ATTR + block.timestamp
+    encoded += FLAG_BLOCK_ATTR + block.prevHash
+    encoded += FLAG_BLOCK_ATTR + block.merkle_root
+    encoded += FLAG_BLOCK_ATTR + block.nonce
+    encoded += FLAG_BLOCK_ATTR + block.hash
+    encoded += FLAG_BLOCK_ATTR
+
+    for tx in block.transactions:
+        encoded += encodeTx(tx, b64encoding=False) + FLAG_TX_ARRAY
+
+    encoded = base64.b64encode(encoded)
+    return encoded
+
+def decodeBlock(data: bytes) -> Block:
+    """Function to convert base64 encoded data into a block object
+
+    Args:
+        data (bytes): the data to convert
+
+    Returns a block object
+    """
+
+    data = base64.b64decode(data)
     
+    if data[:2] != FLAG_BLOCK:
+        print('[Serialization]: Not a block!')
+        return None
+    
+    data = data.split(FLAG_BLOCK_ATTR)
+
+    height = int(data[1].decode())
+    ts = float(data[2].decode())
+    prevHash = data[3]
+    merkle_root = data[4]
+    n = int(data[5].decode())
+    hash = data[6]
+
+    raw_trasactions = data[7].split(FLAG_TX_ARRAY)
+    raw_trasactions = raw_trasactions[:len(raw_trasactions)-1]
+    
+    transactions = [decodeTx(tx, b64decoding=False) for tx in raw_trasactions]
+
+    block = Block(height, prevHash, transactions, timestamp=ts, nonce=n)
+
+    if block.merkle_root != merkle_root:
+        print('[Serialization]: Block contains incorrect merkle root!')
+
+    if block.hash != hash:
+        print('[Serialization]: Block contains incorrect hash!')
+
+    return block
+
+
